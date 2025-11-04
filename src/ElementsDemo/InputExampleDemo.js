72516c52bf8c263fbea694b64c73cf9451cf37fa
171505116@qq.com <171505116@qq.com>
2025-11-04 15:45:58 +0800

    first commit

---
+++ src/ElementsDemo/InputExampleDemo.js
@@ -0,0 +1,28 @@
+
+
+import React from 'react';
+import {Input} from 'react-native-elements';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+function InputExampleDemo(){
+
+
+    return(
+        <View>
+
+        </View>
+    )
+
+}
+
+
+
+export default InputExampleDemo
